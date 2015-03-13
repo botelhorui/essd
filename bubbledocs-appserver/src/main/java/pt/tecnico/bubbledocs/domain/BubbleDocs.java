fee614006ff54c8300aac419a97696267805dc27
botelhorui <botelhorui@gmail.com>
2015-03-13 12:06:49 +0000

    finished BubbleDocs.importSheet for now

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/BubbleDocs.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/BubbleDocs.java
@@ -8,6 +8,7 @@ import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.DateTimeParser;
 import org.joda.time.format.ISODateTimeFormat;
 
+import pt.ist.fenixframework.Atomic;
 import pt.ist.fenixframework.FenixFramework;
 import pt.tecnico.bubbledocs.exception.DifferentUserImportException;
 import pt.tecnico.bubbledocs.exception.UserDoesNotExistException;
@@ -48,6 +49,7 @@ public class BubbleDocs extends BubbleDocs_Base {
 	public boolean hasUser(String username){
 		return getUserByUsername(username) != null;
 	}
+	
 	@Override
     public void addUser(pt.tecnico.bubbledocs.domain.User user) throws UsernameAlreadyExistsException {
         if(hasUser(user.getUsername())){
@@ -64,35 +66,43 @@ public class BubbleDocs extends BubbleDocs_Base {
     	super.removeUser(u);
     }
     
+    @Atomic
     public void importSheet(Document doc,String username){
-    	//if(!doc.getRootElement().getName().equals("SheetData")){ 
-    	// throw end of universe
-    	//}
-    	// TODO
-    	Element root = doc.getRootElement();
-    	Element creator = root.getChild("Creator");
-    	String importUsername = creator.getAttributeValue("username");
-    	if(!importUsername.equals(username))
-    		throw new DifferentUserImportException();
-    	User u = getUserByUsername(username);
-    	if(u == null)
-    		throw new UserDoesNotExistException();
-    	SheetData sd = new SheetData();
-    	sd.init(u, root.getAttributeValue("name"),
-    			Integer.parseInt(root.getAttributeValue("lines")),
-    			Integer.parseInt(root.getAttributeValue("columns")));
-    	DateTimeFormatter dtf = ISODateTimeFormat.dateTime();
-    	sd.setCreationDate(dtf.parseDateTime(root.getAttributeValue("creation-date")));
-    	SheetAccess sa = new SheetAccess();
-    	sa.init(u,sd,true);
-   	
-    	Element cells = root.getChild("Cells");
-    	for(Element celle: cells.getChildren()){
-    		Cell cell = new Cell();
-    		cell.init(sd, 
-    				Integer.parseInt(celle.getAttributeValue("line")),
-    				Integer.parseInt(celle.getAttributeValue("column")),
-    				celle.getAttributeValue("text"));    		
+    	try{
+    		//Every get we use in the xml might return null if the xml is invalid
+	    	Element root = doc.getRootElement();
+	    	Element creator = root.getChild("Creator");
+	    	String importUsername = creator.getAttributeValue("username");
+	    	if(!importUsername.equals(username))
+	    		throw new DifferentUserImportException();
+	    	User u = getUserByUsername(username);
+	    	if(u == null)
+	    		throw new UserDoesNotExistException();
+	    	// import SheetData
+	    	SheetData sd = new SheetData();
+	    	sd.init(u, root.getAttributeValue("name"),
+	    			Integer.parseInt(root.getAttributeValue("lines")),
+	    			Integer.parseInt(root.getAttributeValue("columns")));
+	    	DateTimeFormatter dtf = ISODateTimeFormat.dateTime();
+	    	sd.setCreationDate(dtf.parseDateTime(root.getAttributeValue("creation-date")));
+	    	// create a new SheetAccess for permissions related to the creator
+	    	SheetAccess sa = new SheetAccess();
+	    	sa.init(u,sd,true);
+	   	
+	    	// import all cells in any
+	    	Element cells = root.getChild("Cells");
+	    	for(Element celle: cells.getChildren()){
+	    		Cell cell = new Cell();
+	    		cell.init(sd, 
+	    				Integer.parseInt(celle.getAttributeValue("line")),
+	    				Integer.parseInt(celle.getAttributeValue("column")),
+	    				celle.getAttributeValue("text"));    		
+	    	}
+    	}
+    	catch(NullPointerException e){
+    		System.out.println("Exception while importing a sheet:");
+    		System.out.println(e);
+    		throw e;
     	}
     	
     }
