237f9a027aebd68f73fd9456aacca46fcaae50b1
tatatacs <tatacostaesilva@gmail.com>
2015-03-31 02:33:35 +0100

    closes #1

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
@@ -1,18 +1,45 @@
 package pt.tecnico.bubbledocs.service;
 
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
+import pt.tecnico.bubbledocs.exception.EmptyUsernameException;
+import pt.tecnico.bubbledocs.exception.UnauthorizedOperationException;
+import pt.tecnico.bubbledocs.exception.UserNotInSessionException;
+
+import pt.tecnico.bubbledocs.domain.BubbleDocs;
+import pt.tecnico.bubbledocs.domain.User;
 
 // add needed import declarations
 
 public class CreateUser extends BubbleDocsService {
 	private String token;
+	private String newUsername;
+	private String password;
+	private String name;
+	
 	public CreateUser(String token, String newUsername,String password, String name) {
-		// add code here
-		this.token=token;
+		this.newUsername = newUsername;
+		this.password = password;
+		this.name = name;
+		this.token = token;
 	}
 
 	@Override
 	protected void dispatch() throws BubbleDocsException {
-		// add code here
+		
+		BubbleDocs bd = BubbleDocs.getInstance();
+		User user = bd.getUserByToken(this.token);
+		
+		
+		if(newUsername.equals(""))
+			throw new EmptyUsernameException();
+		
+		if(!(bd.isUserInSession(this.token)))
+			throw new UserNotInSessionException();
+		
+		if(!(user.getUsername().equals("root")))
+			throw new UnauthorizedOperationException();
+		
+		bd.createUser(this.newUsername, this.password, this.name);
+		
 	}
 }
