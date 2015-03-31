6f0f351da817f050fd39b3992701781056e11661
PeMCaeiro <pedro.miguel14@gmail.com>
2015-03-31 01:01:46 +0100

    Finished (?) the AssignReferenceCellTest
    added ReferencedCellOutOfBound
    added CellOutOfBound
    added UserHasNotWriteAccess

--- bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/AssignReferenceCellTest.java
+++ bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/AssignReferenceCellTest.java
@@ -15,6 +15,7 @@ import pt.tecnico.bubbledocs.exception.PositionOutOfBoundsException;
 import pt.tecnico.bubbledocs.exception.UserNotInSessionException;
 import pt.tecnico.bubbledocs.exception.SpreadSheetIdUnknown;
 import pt.tecnico.bubbledocs.exception.UnknownBubbleDocsUserException;
+import pt.tecnico.bubbledocs.exception.UserHasNotWriteAccessException;
 
 
 public class AssignReferenceCellTest extends BubbleDocsServiceTest {
@@ -22,30 +23,47 @@ public class AssignReferenceCellTest extends BubbleDocsServiceTest {
 	// the tokens
 	private String jp; // the token for user jp
 	private String ars; // the token for user ars
+	private String nwp; // the token for user nwp
 	private String root; // the token for user root
+	private String uu = "uu"; // the token for the unknown user
 
+	// the perfect user
 	private static final String USERNAME = "jp";
 	private static final String PASSWORD = "jp#";
+
+	// the no session user
 	private static final String USERNAME2 = "ars";
 	private static final String PASSWORD2 = "ars#";
+
+	// the no write permission user
+	private static final String USERNAME3 = "nwp";
+	private static final String PASSWORD3 = "nwp#";
+
+	// the root user
 	private static final String ROOT_USERNAME = "root";
+
+	private static final int l1 = 1;
+	private static final int c1 = 1;
+	private static final int l2 = 1;
+	private static final int c2 = 2;
+	private static final int l3 = 20;
+	private static final int c3 = 20;
+	private static final int l4 = 20;
+	private static final int c4 = 21;
+
+	// the valid cell id & referenced cell id
+	private static final String cell = l1 + ";" + c1;
+	private static final String reference = l2 + ";" + c2;
+
+	// the invalid cell id &  referenced cell id
+	private static final String invalidCell = l3 + ";" + c3;
+	private static final String invalidReference = l4 + ";" + c4;
 
 	private static final String SPREADNAME = "testsheet";
 	private static final int lines = 10;
 	private static final int columns = 10;
 
-	private static final int l1 = 1;
-	private static final int c1 = 1;
-
-	private static final int l2 = 1;
-	private static final int c2 = 2;
-
-	// valid cell id
-	private static final String cell = l1 + ";" + c1;
-
-	// valid referenced cell id
-	private static final String reference = l2 + ";" + c2;
-
+	// spread sheet id
 	private int spread_id;
 
 	@Override
@@ -53,12 +71,13 @@ public class AssignReferenceCellTest extends BubbleDocsServiceTest {
 
 		createUser(USERNAME,PASSWORD,"Joao Pereira");
 		createUser(USERNAME2,PASSWORD2,"Armenio Rol Silva");
+		createUser(USERNAME3,PASSWORD3,"Jaquim Ambrosio");
 
 		root = addUserToSession(ROOT_USERNAME);
-
 		jp = addUserToSession(USERNAME);
+		ars = addUserToSession(USERNAME2);	
+		nwp = addUserToSession(USERNAME3);
 
-		ars = addUserToSession(USERNAME2);		
 		removeUserFromSession(ars);
 
 		User jp_user = getUserFromUsername(jp);
@@ -102,6 +121,8 @@ public class AssignReferenceCellTest extends BubbleDocsServiceTest {
 
 		// valid referenced cell id = reference
 
+		// valid spread id = spread_id
+
 		// user not in session, ars
 
 		AssignReferenceCell service = new AssignReferenceCell( ars , spread_id , cell , reference );
@@ -117,7 +138,9 @@ public class AssignReferenceCellTest extends BubbleDocsServiceTest {
 
 		// valid referenced cell id = reference
 
-		// spread doesn't exist spread_id + 1000
+		// invalid spread id = spread_id + 1000
+
+		// user exists & is in session & has permission to write = jp
 
 		AssignReferenceCell service = new AssignReferenceCell( jp , spread_id + 1000 , cell , reference );
 		service.execute();
@@ -132,9 +155,62 @@ public class AssignReferenceCellTest extends BubbleDocsServiceTest {
 
 		// valid referenced cell id = reference
 
-		// spread doesn't exist spread_id + 1000
+		// valid spread id = spread_id
 
-		AssignReferenceCell service = new AssignReferenceCell( jp , spread_id + 1000 , cell , reference );
+		// unknown bubbledocs user = uu
+
+		AssignReferenceCell service = new AssignReferenceCell( uu , spread_id , cell , reference );
+		service.execute();
+
+	}
+
+	@Test(expected = UserHasNotWriteAccessException.class)
+
+	public void userHasNotWriteAccess() {
+
+		// valid cell id = cell
+
+		// valid referenced cell id = reference
+
+		// valid spread id = spread_id
+
+		// user has session doesn't have write access = nwa
+
+		AssignReferenceCell service = new AssignReferenceCell( nwa , spread_id , cell , reference );
+		service.execute();
+
+	}
+
+	@Test(expected = PositionOutOfBoundsException.class)
+
+	public void cellOutOfBounds() {
+
+		// invalid cell id = invalidCell
+
+		// valid referenced cell id = reference
+
+		// valid spread id = spread_id
+
+		// user exists & is in session & has permission to write = jp
+
+		AssignReferenceCell service = new AssignReferenceCell( jp , spread_id , invalidCell , reference );
+		service.execute();
+
+	}
+
+	@Test(expected = PositionOutOfBoundsException.class)
+
+	public void referencedCellOutOFBound() {
+
+		// valid cell id = cell
+
+		// invalid referenced cell id = invalidReference
+
+		// valid spread id = spread_id
+
+		// user exists & is in session & has permission to write = jp
+
+		AssignReferenceCell service = new AssignReferenceCell( jp , spread_id , cell , invalidReference );
 		service.execute();
 
 	}
