afe89c8de0e19cee0f813e8b895936f83b6e3e24
PeMCaeiro <pedro.miguel14@gmail.com>
2015-04-15 02:54:02 +0100

    closes #34

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exception/CannotStoreDocumentException.java
@@ -0,0 +1,10 @@
+package pt.tecnico.bubbledocs.exception;
+
+public class CannotStoreDocumentException extends BubbleDocsException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exception/RemoteInvocationException.java
@@ -0,0 +1,10 @@
+package pt.tecnico.bubbledocs.exception;
+
+public class RemoteInvocationException extends BubbleDocsException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
