8ac75bb42c39cbe5a5755e46c865a53a91e0fe03
PeMCaeiro <pedro.miguel14@gmail.com>
2015-04-26 22:01:57 +0100

    closes #55

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/GetUsername4TokenService.java
@@ -0,0 +1,24 @@
+package pt.tecnico.bubbledocs.service;
+
+import pt.tecnico.bubbledocs.domain.BubbleDocs;
+import pt.tecnico.bubbledocs.exception.BubbleDocsException;
+
+public class GetUsername4TokenService extends BubbleDocsService{
+	private String _token, _username;
+	
+	public GetUsername4TokenService(String token) {
+		this._token = token;
+	}
+	
+	@Override
+    protected void dispatch() throws BubbleDocsException{
+		BubbleDocs bd = BubbleDocs.getInstance();
+		
+		_username = bd.getUsernameFromToken(_token);
+    }
+	
+	public String getUsername() {
+		return _username;
+	}
+    
+}
